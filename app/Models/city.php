bb4860d680bd09e1f0a2356eba9c20f9585f0213
Iqrakhan805 <iqranawaz5566@gmail.com>
2025-10-30 07:49:11 -0700

    uploaded

--- app/Models/city.php
+++ app/Models/city.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-class city extends Model
+class City extends Model
 {
     use HasFactory;
     protected $table = 'city'; 
